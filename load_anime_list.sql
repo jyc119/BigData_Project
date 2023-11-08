b73b36652e4c9f12f0343f7c476d1dbc4139d2b0
Jordan Chin <60002815+jyc119@users.noreply.github.com>
2023-11-08 18:12:18 -0500

    Delete load_anime_list.sql

--- load_anime_list.sql
+++
@@ -1,4 +0,0 @@
-COPY anime_list(anime_id, title, title_english,title_japanese,title_synonym,image_url, type, source, episodes, status, airing, airing_string, aired, duration, rating, score, scored_by, rank, popularity, members, favorites, background, premiered,broadcast, related, producer, licensor, studio, genre, opening_theme, ending_theme)
-FROM 'C:\Users\jorda\OneDrive\Desktop\CS RIT\Intro to Big Data\Project\AnimeList.csv'
-DELIMITER ','
-CSV HEADER;
